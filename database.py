f293201a129ef8591ca922b15405c9e76211cb63
Pathan-Saif <itlt2307@glbitm.ac.in>
2026-01-24 00:11:57 +0530

    Use env variables for database config

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:pathansaif367@localhost:5432/recommender"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
