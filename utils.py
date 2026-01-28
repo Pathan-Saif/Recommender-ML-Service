b5a1dd7ff58b70e1884238d737fadc01300a0ad6
Pathan-Saif <itlt2307@glbitm.ac.in>
2026-01-28 22:35:05 +0530

    final updatedCode

--- utils.py
+++ utils.py
@@ -1,22 +1,6 @@
-# from datetime import datetime
-
-# def map_event_to_weight(event_type: str):
-#     mapping = {
-#         "view": 0.5,
-#         "click": 1.0,
-#         "cart": 2.0,
-#         "purchase": 3.0
-#     }
-#     return mapping.get(event_type, 0.5)
-
-
 def map_event_to_weight(event_type: str):
     mapping = {"view": 0.5, "click": 1.0, "cart": 2.0, "purchase": 3.0}
     return mapping.get(event_type, 0.5)
 
 
 
-
-
-# PS C:\Users\HP\desktop\ml-service> .\venv\Scripts\activate
-# (venv) PS C:\Users\HP\desktop\ml-service> uvicorn app:app --reload
